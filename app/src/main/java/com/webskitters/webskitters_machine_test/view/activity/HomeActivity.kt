7eeb2636eeeb59cc0cad8df6587fd7e2da606d4d
soumoq <soumojyotiganguly270@gmail.com>
2021-06-06 18:39:06 +0530

    Implement drawer nav component

--- app/src/main/java/com/webskitters/webskitters_machine_test/view/activity/HomeActivity.kt
+++ app/src/main/java/com/webskitters/webskitters_machine_test/view/activity/HomeActivity.kt
@@ -1,12 +1,31 @@
 package com.webskitters.webskitters_machine_test.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.webskitters.webskitters_machine_test.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+//        NavigationUI.setupActionBarWithNavController(this, navController)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
 }
